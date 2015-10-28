39582c8345f065318d4b4deab75b5eb6e15f0721
Andréas <pinbouen.andreas@gmail.com>
2015-10-28 14:57:25 +0900

    [b0.10.0b] Preview of profile picture ADDED - Preview of profile picture - Design adjustments

--- profile.php
+++ profile.php
@@ -2,9 +2,6 @@
 session_start();
 require "functions/db_connect.php";
 $db = PDOFactory::getConnection();
-$queryActiveRooms = $db->query("SELECT * FROM rooms r
-								JOIN user u ON r.room_creator = u.user_token
-								WHERE room_active = 1");
 
 $userToken = $_GET["id"];
 
@@ -16,63 +13,106 @@ if(isset($_GET["lang"])){
 } else {
 	header("Location:home.php?lang=en");
 }
-
 if(isset($_POST["submit"])){
+	$newPseudo = addslashes($_POST["username"]);
+	$newBio = addslashes($_POST["bio"]);
 	// Uploading the profile picture on the folder
 	if($_FILES["profile-picture"]["name"]){
-		$picture = $_SESSION["token"].".".pathinfo($_FILES["profile-picture"]["name"], PATHINFO_EXTENSION);
-		move_uploaded_file($_FILES["profile-picture"]["tmp_name"], "profile-pictures/".$picture);
+		$pictureExtension = pathinfo($_FILES["profile-picture"]["name"], PATHINFO_EXTENSION);
+		if($pictureExtension != "png" && $pictureExtension != "jpg"){
+			$message = "Invalid format";
+		} else {
+			if($_FILES["profile-picture"]["size"] > (3072000)){
+				$message = "The size is too big. 3MB max.";
+				//Writing in the table the modifications
+				$edit = $db->query("UPDATE user
+									SET user_pseudo = '$newPseudo',
+									user_bio = '$newBio'
+									WHERE user_token = '$userToken'");
+			} else {
+				$picture = $_SESSION["token"].".".pathinfo($_FILES["profile-picture"]["name"], PATHINFO_EXTENSION);
+				move_uploaded_file($_FILES["profile-picture"]["tmp_name"], "profile-pictures/".$picture);
+				//Writing in the table the modifications
+				$edit = $db->query("UPDATE user
+									SET user_pseudo = '$newPseudo',
+									user_bio = '$newBio',
+									user_pp = '$picture'
+									WHERE user_token = '$userToken'");
+			}
+		}
+	} else {
+		$edit = $db->query("UPDATE user
+													SET user_pseudo = '$newPseudo',
+													user_bio = '$newBio'
+													WHERE user_token = '$userToken'");
 	}
-
-	//Writing in the table the modifications
-	$newPseudo = $_POST["username"];
-	$newBio = $_POST["bio"];
-	$edit = $db->query("UPDATE user
-						SET user_pseudo = '$newPseudo',
-							user_bio = '$newBio',
-							user_pp = '$picture'
-						WHERE user_token = '$userToken'");
 }
+
+$queryActiveRooms = $db->query("SELECT * FROM rooms r
+								JOIN user u ON r.room_creator = u.user_token
+								WHERE room_active = 1");
 ?>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<title>My Profile</title>
 		<?php include "styles.php";?>
+		<link rel="stylesheet" href="assets/css/fileinput.min.css">
 	</head>
 	<body>
 		<?php include "nav.php";?>
 		<div class="main col-lg-12">
-			<form action="" class="form-horizontal" method="post" enctype="multipart/form-data">
+			<form action="profile.php?id=<?php echo $userToken;?>&lang=<?php echo $_SESSION["lang"];?>" class="form-horizontal" method="post" enctype="multipart/form-data">
 				<div class="form-group">
 					<label for="username" class="col-sm-3 control-label"><?php echo $lang["display_name"];?></label>
 					<div class="col-sm-6">
-						<input type="text" name="username" class="form-control" aria-describedby="username-tip" value="<?php echo $userDetails["user_pseudo"];?>">
+						<input type="text" name="username" class="form-control" aria-describedby="username-tip" value="<?php echo stripslashes($userDetails["user_pseudo"]);?>">
 						<span class="help-block" id="username-tip"><?php echo $lang["display_name_tip"];?></span>
 					</div>
 				</div>
 				<div class="form-group">
 					<label for="profile-picture" class="col-sm-3 control-label"><?php echo $lang["profile_picture"];?></label>
 					<div class="col-sm-6">
-						<div id="profile-picture-sample">
-							<img src="<?php echo $ppAdresss;?>" alt="" style="width:inherit;">
+						<div id="kv-avatar-errors" class="center-block" style="width:800px;display:none;"></div>
+						<div class="kv-avatar">
+							<input type="file" id="avatar" name="profile-picture" class="file-loading">
 						</div>
-						<input type="file" name="profile-picture" class="control-label">
+						<!--<div id="profile-picture-sample">
+<img src="<?php echo $ppAdresss;?>" alt="" style="width:inherit;">
+</div>-->
 						<span class="help-block" id="username-tip"><?php echo $lang["profile_picture_formats"];?></span>
 					</div>
 				</div>
 				<div class="form-group">
 					<label for="bio" class="col-lg-3 control-label"><?php echo $lang["bio"];?></label>
 					<div class="col-lg-6">
-						<textarea rows="5" name="bio" class="form-control" aria-describedby="bio-tip" style="background-color:inherit; border:2px #444 solid;"><?php echo $userDetails["user_bio"];?></textarea>
+						<textarea rows="5" maxlength="400" name="bio" class="form-control" aria-describedby="bio-tip" style="background-color:inherit; border:2px #444 solid; color:white;"><?php echo stripslashes($userDetails["user_bio"]);?></textarea>
 						<span class="help-block" id="bio-tip"><?php echo $lang["bio_tip"];?></span>
 					</div>
 				</div>
-				<div class="col-lg-offset-3">
-					<input type="submit" class="btn btn-primary" name="submit" value="<?php echo $lang["save_changes"];?>">
+				<div class="col-lg-offset-2 col-lg-8">
+					<input type="submit" class="btn btn-primary btn-block" name="submit" value="<?php echo $lang["save_changes"];?>">
 				</div>
 			</form>
 		</div>
 		<?php include "scripts.php";?>
+		<script src="assets/js/fileinput.min.js"></script>
+		<script>
+			$("#avatar").fileinput({
+				overwriteInitial: true,
+				maxFileSize: 3000,
+				showClose: false,
+				showCaption: false,
+				browseLabel: '',
+				removeLabel: '',
+				browseIcon: '<i class="glyphicon glyphicon-folder-open"></i>',
+				removeTitle: 'Cancel or reset changes',
+				elErrorContainers: '#kv-avatar-errors',
+				msgErrorClass: 'alert alert-block alert-danger',
+				defaultPreviewContent: '<img src="<?php echo $ppAdresss;?>" style="width:118px;">',
+				layoutTemplates: {main2: '{preview} {browse}' },
+				allowedFileExtensions: ["jpg", "png"]
+			});
+		</script>
 	</body>
 </html>
--- test.php
+++ test.php
@@ -1,4 +1,41 @@
 <?php
-date_default_timezone_set('UTC');
-echo date('Y-m-d H:i:s', time());
+session_start();
+require "functions/db_connect.php";
+$db = PDOFactory::getConnection();
+$queryActiveRooms = $db->query("SELECT * FROM rooms r
+								JOIN user u ON r.room_creator = u.user_token
+								WHERE room_active = 1");
+
 ?>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Strawberry Music Streamer</title>
+		<?php include "styles.php";?>
+	</head>
+	<body>
+		<div class="main">
+			<span class="timer"></span><br>
+			<span class="presses"></span><br>
+			<span class="mean"></span><br><br>
+			<span class="letters"></span>
+		</div>
+		<?php include "scripts.php";?>
+		<script>
+			$(document).ready(function(){
+				var timer = new Date;
+				setInterval(function(){
+					$(".timer").text((new Date - timer) / 1000);
+					$(".mean").html(presses / $(".timer").text());
+				}, 1000);
+				var presses = 0;
+				$(document).keypress(function(event){
+					presses++;
+					$(".presses").html(presses);
+					$(".mean").html(presses / $(".timer").text());
+					$(".letters").append(event.keyCode+", ");
+				})
+			})
+		</script>
+	</body>
+</html>
