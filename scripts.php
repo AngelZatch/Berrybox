f72a46c5f71d549d5a9c5f6d78a6744533d74241
Andréas <pinbouen.andreas@gmail.com>
2015-11-09 19:31:04 +0900

    [b0.11.0a] Quick now playing fix FIXED - Now playing message will now display correctly
    REMOVED
    - jQuery UI links as it's still WIP.

--- scripts.php
+++ scripts.php
@@ -1,5 +1,5 @@
 <script src="assets/js/jquery-2.1.4.min.js"></script>
-<script src="assets/js/jquery-ui.min.js"></script>
+<!--<script src="assets/js/jquery-ui.min.js"></script>-->
 <script src="bootstrap/js/bootstrap.js"></script>
 <script src="assets/js/moment.js"></script>
 <script src="assets/js/moment-timezone.js"></script>
