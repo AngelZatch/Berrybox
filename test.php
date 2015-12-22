ffe9a4e873a40afebb038b234002c23ced4bc7f2
AngelZatch <pinbouen.andreas@gmail.com>
2015-12-23 02:11:03 +0900

    Reset of test page

--- test.php
+++ test.php
@@ -15,27 +15,7 @@ $queryActiveRooms = $db->query("SELECT * FROM rooms r
 	</head>
 	<body>
 		<div class="main">
-			<?php
-			/*$played = $db->query("UPDATE roomHistory_bqtaygnaaxgdr7g
-					SET video_status='5'
-					WHERE history_link = 'KLqZ4AAypFE'");*/
 
-			$nextVideoState = $db->query("SELECT video_status, room_history_id
-											FROM roomHistory_bqtaygnaaxgdr7g
-											WHERE room_history_id =
-												(SELECT room_history_id
-													FROM roomHistory_bqtaygnaaxgdr7g
-													WHERE history_link='tzUuqu9vqCs'
-													AND video_status='2')+1")->fetch(PDO::FETCH_ASSOC);
-			//$played = $db->query("SELECT 26")->fetch(PDO::FETCH_ASSOC);
-			echo $nextVideoState["video_status"];
-			if($nextVideoState["video_status"] > 2){
-				$confirmIgnore = $db->query("UPDATE roomHistory_bqtaygnaaxgdr7g
-											SET video_status = '2'
-											WHERE room_history_id = '$nextVideoState[room_history_id]'")
-			}
-			/*echo $played;*/
-			?>
 		</div>
 		<?php include "scripts.php";?>
 	</body>
