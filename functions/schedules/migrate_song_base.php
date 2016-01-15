3c72a50d723103645b4ad73df6cbc116d8c5f14d
AngelZatch <pinbouen.andreas@gmail.com>
2016-01-15 09:50:51 +0900

    Correcting migration script

--- functions/schedules/migrate_song_base.php
+++ functions/schedules/migrate_song_base.php
@@ -4,12 +4,12 @@ $db = PDOFactory::getConnection();
 
 $tokens = array("1FE9IC3FV0FISQ3", "5F9FVNP3SYK9XGD", "5T1Z8L2B6EJ4QX8", "8BRD9ATZCBRSDKD", '9FC00R2JLPY5D8O', "55LEQWBIXRWSXLS", "BQ92DPN8D356KB5", "DTPE0X0VSANKOY8", "FEJBFEJSRSTG24N", "HAMSOHVI0EEWZRP", "HHKMO1TM3Z90B4Q", "HNF20X4FFH6XNK1", "KRTVXFPPV7C153T", "MMKMW3R9U48SOSW", "MU3231U6HMAJJ6Y", "OFNQ9ZLBSN28GSV", "QR4G8OCOG0LW93T", "TTOAHAFH43DYRSK", "V849W0FWSSGSAH1", "VNSHP2XAIS3GV6E", "ZSIS86PQQFGBQA9");
 
-for($i = 0; $i = sizeof($tokens), $i++){
+for($i = 0; $i = sizeof($tokens); $i++){
 	$querySongs = $db->query("SELECT history_link, video_name FROM roomHistory_$tokens[$i]");
 
-while($song = $querySongs->fetch(PDO::FETCH_ASSOC)){
-	$edit = $db->query("UPDATE song_base SET video_name = '$song[videoname]' WHERE link = $song[history_link]");
-}
+	while($song = $querySongs->fetch(PDO::FETCH_ASSOC)){
+		$edit = $db->query("UPDATE song_base SET video_name = '$song[videoname]' WHERE link = $song[history_link]");
+	}
 
 }
 ?>
