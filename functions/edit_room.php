ed703e6e7c96d042bd36971f4a99597867e53441
AngelZatch <pinbouen.andreas@gmail.com>
2016-02-01 18:47:59 +0900

    Protection level setting in box
    ADDED
    - Can now change the protection level of a box while inside it.

--- functions/edit_room.php
+++ functions/edit_room.php
@@ -4,15 +4,18 @@ include "tools.php";
 $db = PDOFactory::getConnection();
 $type = $_POST["type"];
 $language = $_POST["language"];
+$protect = $_POST["protect"];
 $description = $_POST["description"];
 $roomToken = $_POST["roomToken"];
 
 try{
 	$db->beginTransaction();
+	$oldProtectValue = $db->query("SELECT room_protection FROM rooms WHERE room_token = '$roomToken'")->fetch(PDO::FETCH_ASSOC);
 	$editRoom = $db->query("UPDATE rooms
-								SET room_type = '$type', room_lang = '$language', room_description = '$description'
+								SET room_type = '$type', room_lang = '$language', room_protection = '$protect', room_description = '$description'
 								WHERE room_token = '$roomToken'");
 	$db->commit();
+	echo $oldProtectValue["room_protection"];
 } catch(PDOException $e){
 	$db->rollBack();
 }
