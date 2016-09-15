0770d956415728d62dc3cd7cabae10e664b43298
AngelZatch <pinbouen.andreas@gmail.com>
2016-09-15 16:54:16 +0200

    [FIX] Close/Reopen box

--- functions/reopen_room.php
+++ functions/reopen_room.php
@@ -1,9 +1,10 @@
 <?php
 include "db_connect.php";
 $db = PDOFactory::getConnection();
-$roomToken = $_POST["roomToken"];
+
+$box_token = $_POST["box_token"];
 
 $reopen = $db->query("UPDATE rooms
 					SET room_active = '1'
-					WHERE box_token='$roomToken'")->fetch(PDO::FETCH_ASSOC);
+					WHERE box_token='$box_token'");
 ?>
