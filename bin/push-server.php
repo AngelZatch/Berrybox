259bc06a7e5aaf170fb75f21a84b8316d32dabce
AngelZatch <pinbouen.andreas@gmail.com>
2016-12-12 17:35:47 +0100

    Listen type

--- bin/push-server.php
+++ bin/push-server.php
@@ -10,7 +10,7 @@ $pull->bind('tcp://127.0.0.1:5555');
 $pull->on('message', array($pusher, 'onEntry'));
 
 $webSock = new React\Socket\Server($loop);
-$webSock->listen(8080);
+$webSock->listen(8080, '0.0.0.0');
 $webServer = new Ratchet\Server\IoServer(
 	new Ratchet\Http\HttpServer(
 		new Ratchet\WebSocket\WsServer(
